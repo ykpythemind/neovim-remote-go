7f86219ca6ef89430e52dc975f0a3e0dd9d36484
ykpythemind <yukibukiyou@gmail.com>
2022-01-06 01:32:50 +0900

    ok

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/neovim/go-client/nvim"
+)
+
+func main() {
+	var remoteWait string
+	flag.StringVar(&remoteWait, "remote-wait", "", "wait remote")
+	flag.Parse()
+
+	address := os.Getenv("NVIM_LISTEN_ADDRESS")
+
+	if address == "" {
+		log.Fatal("missing env NVIM_LISTEN_ADDRESS")
+	}
+
+	runner, err := NewRunner(address, remoteWait)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer runner.Close()
+
+	if err = runner.Do(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewRunner(address string, remoteWait string) (*Runner, error) {
+	nvim, err := nvim.Dial(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial %s %s", address, err)
+	}
+
+	// TODO: nvim.SetClientInfo()
+
+	if remoteWait == "" {
+		return nil, errors.New("remote wait is blank. now it is error")
+	}
+
+	return &Runner{
+		nvim:       nvim,
+		remoteWait: remoteWait,
+		waitCount:  0,
+	}, nil
+}
+
+type Runner struct {
+	nvim       *nvim.Nvim
+	remoteWait string // now it is file.
+	// TODO: files string[]
+	waitCount int
+	m         sync.Mutex
+}
+
+func (r *Runner) Do() error {
+	waitCh := make(chan struct{}, 1)
+
+	if err := r.nvim.Command("split"); err != nil {
+		return err
+	}
+
+	cmd := fmt.Sprintf("edit %s", r.remoteWait)
+	if err := r.nvim.Command(cmd); err != nil {
+		return err
+	}
+
+	if r.wait() {
+		// set wait for current buffer
+		_, err := r.nvim.CurrentBuffer()
+		if err != nil {
+			return err
+		}
+
+		c := strconv.Itoa(r.nvim.ChannelID())
+
+		// need to Batch execute???
+
+		r.addWait(+1)
+		if err := r.nvim.Subscribe("BufDelete"); err != nil {
+			return err
+		}
+
+		if err := r.nvim.RegisterHandler("BufDelete", func(args ...interface{}) {
+			fmt.Println("received bufdelete!!!!!")
+			waitCh <- struct{}{}
+		}); err != nil {
+			return err
+		}
+
+		if err := r.nvim.Command("augroup nvr"); err != nil {
+			return err
+		}
+
+		if err := r.nvim.Command(fmt.Sprintf(`autocmd BufDelete <buffer> silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
+			return err
+		}
+
+		if err := r.nvim.Command(fmt.Sprintf(`autocmd VimLeave silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
+			return err
+		}
+
+		// TODO? self.server.command('autocmd VimLeave * if exists("v:exiting") && v:exiting > 0 | silent! call rpcnotify({}, "Exit", v:exiting) | endif'.format(chanid))
+
+		if err := r.nvim.Command("augroup END"); err != nil {
+			return err
+		}
+	}
+
+	// wait logic...
+
+loop:
+	for {
+		select {
+		case <-waitCh:
+			r.addWait(-1)
+		default:
+			// if not wait...
+
+			if r.waitCount < 1 {
+				break loop
+			}
+		}
+	}
+
+	return nil
+}
+
+func (r *Runner) Close() error {
+	return r.nvim.Close()
+}
+
+func (r *Runner) wait() bool {
+	return r.remoteWait != ""
+}
+
+func (r *Runner) addWait(n int) {
+	r.m.Lock()
+	r.waitCount += n
+	r.m.Unlock()
+}
