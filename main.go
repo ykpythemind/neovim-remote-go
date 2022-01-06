9bd04447947f170abef25196177d1e63db94fee5
ykpythemind <yukibukiyou@gmail.com>
2022-01-07 00:55:48 +0900

    ok

--- main.go
+++ main.go
@@ -16,19 +16,15 @@ import (
 )
 
 func main() {
-	var remoteWait string
+	var remoteWait bool
 	var debug bool
-	flag.StringVar(&remoteWait, "remote-wait", "", "wait remote")
+	flag.BoolVar(&remoteWait, "remote-wait", false, "wait remote")
 	flag.BoolVar(&debug, "debug", false, "debug")
 	flag.Parse()
 
 	address := os.Getenv("NVIM_LISTEN_ADDRESS")
 
-	// if address == "" {
-	// 	log.Fatal("missing env NVIM_LISTEN_ADDRESS")
-	// }
-
-	runner, err := NewRunner(address, remoteWait, debug)
+	runner, err := NewRunner(address, flag.Args(), remoteWait, debug)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,13 +38,11 @@ func main() {
 	}
 }
 
-func NewRunner(address string, remoteWait string, debug bool) (*Runner, error) {
-	if remoteWait == "" {
-		return nil, errors.New("remote wait is blank. now it is error")
-	}
-
+func NewRunner(address string, files []string, remoteWait bool, debug bool) (*Runner, error) {
 	return &Runner{
+		address:    address,
 		remoteWait: remoteWait,
+		files:      files,
 		waitCount:  0,
 		debug:      debug,
 	}, nil
@@ -56,11 +50,11 @@ func NewRunner(address string, remoteWait string, debug bool) (*Runner, error) {
 
 type Runner struct {
 	address    string
-	remoteWait string // now it is file.
-	// TODO: files string[]
-	waitCount int
-	debug     bool
-	m         sync.Mutex
+	remoteWait bool
+	files      []string
+	waitCount  int
+	debug      bool
+	m          sync.Mutex
 }
 
 func (r *Runner) Do() error {
@@ -87,47 +81,60 @@ func (r *Runner) Do() error {
 		return err
 	}
 
-	// TODO: flag.Args() is file list
-	file := r.remoteWait
-	cmd := fmt.Sprintf("edit %s", file)
-	if err := nv.Command(cmd); err != nil {
-		return err
+	for i, file := range r.files {
+		editcmd := "edit" // TODO: depends on flag
+
+		if i == 0 {
+			// if first, create new buffer with edit (???)
+			editcmd = "edit"
+		}
+		cmd := fmt.Sprintf("%s %s", editcmd, file)
+
+		if err := nv.Command(cmd); err != nil {
+			return err
+		}
+
+		if r.wait() {
+			// set wait for current buffer
+			_, err := nv.CurrentBuffer()
+			if err != nil {
+				return err
+			}
+
+			c := strconv.Itoa(nv.ChannelID())
+
+			// FIXME: need to Batch execute???
+
+			r.addWait(+1)
+
+			if err := nv.RegisterHandler("BufDelete", func(args ...interface{}) {
+				if r.debug {
+					fmt.Println("received BufDelete")
+				}
+				waitCh <- struct{}{}
+			}); err != nil {
+				return err
+			}
+
+			if err := nv.Command("augroup nvr"); err != nil {
+				return err
+			}
+
+			if err := nv.Command(fmt.Sprintf(`autocmd BufDelete <buffer> silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
+				return err
+			}
+
+			// TODO? もとの実装にはある self.server.command('autocmd VimLeave * if exists("v:exiting") && v:exiting > 0 | silent! call rpcnotify({}, "Exit", v:exiting) | endif'.format(chanid))
+
+			if err := nv.Command("augroup END"); err != nil {
+				return err
+			}
+		}
 	}
 
-	if r.wait() {
-		// set wait for current buffer
-		_, err := nv.CurrentBuffer()
-		if err != nil {
-			return err
-		}
-
-		c := strconv.Itoa(nv.ChannelID())
-
-		// FIXME: need to Batch execute???
-
-		r.addWait(+1)
-
-		if err := nv.RegisterHandler("BufDelete", func(args ...interface{}) {
-			if r.debug {
-				fmt.Println("received bufdelete!!!!!")
-			}
-			waitCh <- struct{}{}
-		}); err != nil {
-			return err
-		}
-
-		if err := nv.Command("augroup nvr"); err != nil {
-			return err
-		}
-
-		if err := nv.Command(fmt.Sprintf(`autocmd BufDelete <buffer> silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
-			return err
-		}
-
-		// TODO? もとの実装にはある self.server.command('autocmd VimLeave * if exists("v:exiting") && v:exiting > 0 | silent! call rpcnotify({}, "Exit", v:exiting) | endif'.format(chanid))
-
-		if err := nv.Command("augroup END"); err != nil {
-			return err
+	if r.waitCount > 0 {
+		if r.debug {
+			fmt.Println("waiting...")
 		}
 	}
 
@@ -148,8 +155,21 @@ loop:
 	return nil
 }
 
+func (r *Runner) doFilenameEscapedCommand(nv *nvim.Nvim, cmd, path string) error {
+	// TODO: escape filename for nvim
+	// path = self.server.funcs.fnameescape(path)
+
+	// TODO: shortmess
+	// shortmess = self.server.options['shortmess']
+	// self.server.options['shortmess'] = shortmess.replace('F', '')
+	// self.server.command('{} {}'.format(cmd, path))
+	// self.server.options['shortmess'] = shortmess
+
+	return nv.Command(fmt.Sprintf("%s %s", cmd, path))
+}
+
 func (r *Runner) wait() bool {
-	return r.remoteWait != ""
+	return r.remoteWait == true
 }
 
 func (r *Runner) addWait(n int) {
@@ -179,6 +199,5 @@ func (r *Runner) startNewNvim() error {
 	newArgs := make([]string, len(args)+1)
 	copy(newArgs[1:], args)
 	newArgs[0] = binary
-	fmt.Println("newArgs", newArgs)
 	return syscall.Exec(binary, newArgs, env)
 }
