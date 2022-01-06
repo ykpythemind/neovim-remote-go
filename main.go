36b4d87ea57d1fa2d0b11b0e87069633c04ff5b9
ykpythemind <yukibukiyou@gmail.com>
2022-01-07 00:29:59 +0900

    impl startNewNvim

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"sync"
+	"syscall"
 
 	"github.com/neovim/go-client/nvim"
 )
@@ -21,35 +24,30 @@ func main() {
 
 	address := os.Getenv("NVIM_LISTEN_ADDRESS")
 
-	if address == "" {
-		log.Fatal("missing env NVIM_LISTEN_ADDRESS")
-	}
+	// if address == "" {
+	// 	log.Fatal("missing env NVIM_LISTEN_ADDRESS")
+	// }
 
 	runner, err := NewRunner(address, remoteWait, debug)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer runner.Close()
 
 	if err = runner.Do(); err != nil {
+		if debug {
+			fmt.Println("runner fail")
+		}
+
 		log.Fatal(err)
 	}
 }
 
 func NewRunner(address string, remoteWait string, debug bool) (*Runner, error) {
-	nvim, err := nvim.Dial(address)
-	if err != nil {
-		return nil, fmt.Errorf("failed to dial %s %s", address, err)
-	}
-
-	// TODO: nvim.SetClientInfo()
-
 	if remoteWait == "" {
 		return nil, errors.New("remote wait is blank. now it is error")
 	}
 
 	return &Runner{
-		nvim:       nvim,
 		remoteWait: remoteWait,
 		waitCount:  0,
 		debug:      debug,
@@ -57,7 +55,7 @@ func NewRunner(address string, remoteWait string, debug bool) (*Runner, error) {
 }
 
 type Runner struct {
-	nvim       *nvim.Nvim
+	address    string
 	remoteWait string // now it is file.
 	// TODO: files string[]
 	waitCount int
@@ -68,34 +66,48 @@ type Runner struct {
 func (r *Runner) Do() error {
 	waitCh := make(chan struct{}, 1)
 
-	if err := r.nvim.Command("split"); err != nil {
+	if r.address == "" {
+		return r.startNewNvim()
+	}
+
+	nv, err := nvim.Dial(r.address)
+	if err != nil {
+		var e net.Error
+		if errors.As(err, &e) {
+			return r.startNewNvim()
+		} else {
+			return fmt.Errorf("failed to dial %s: %w", r.address, err)
+		}
+	}
+	defer nv.Close()
+
+	// TODO: nv.SetClientInfo("neovim-remote-go")
+
+	if err := nv.Command("split"); err != nil {
 		return err
 	}
 
 	// TODO: flag.Args() is file list
 	file := r.remoteWait
 	cmd := fmt.Sprintf("edit %s", file)
-	if err := r.nvim.Command(cmd); err != nil {
+	if err := nv.Command(cmd); err != nil {
 		return err
 	}
 
 	if r.wait() {
 		// set wait for current buffer
-		_, err := r.nvim.CurrentBuffer()
+		_, err := nv.CurrentBuffer()
 		if err != nil {
 			return err
 		}
 
-		c := strconv.Itoa(r.nvim.ChannelID())
+		c := strconv.Itoa(nv.ChannelID())
 
-		// need to Batch execute???
+		// FIXME: need to Batch execute???
 
 		r.addWait(+1)
-		// if err := r.nvim.Subscribe("BufDelete"); err != nil {
-		// 	return err
-		// }
 
-		if err := r.nvim.RegisterHandler("BufDelete", func(args ...interface{}) {
+		if err := nv.RegisterHandler("BufDelete", func(args ...interface{}) {
 			if r.debug {
 				fmt.Println("received bufdelete!!!!!")
 			}
@@ -104,26 +116,20 @@ func (r *Runner) Do() error {
 			return err
 		}
 
-		if err := r.nvim.Command("augroup nvr"); err != nil {
+		if err := nv.Command("augroup nvr"); err != nil {
 			return err
 		}
 
-		if err := r.nvim.Command(fmt.Sprintf(`autocmd BufDelete <buffer> silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
+		if err := nv.Command(fmt.Sprintf(`autocmd BufDelete <buffer> silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
 			return err
 		}
 
-		// if err := r.nvim.Command(fmt.Sprintf(`autocmd VimLeave silent! call rpcnotify(%s, "BufDelete")`, c)); err != nil {
-		// 	return err
-		// }
+		// TODO? もとの実装にはある self.server.command('autocmd VimLeave * if exists("v:exiting") && v:exiting > 0 | silent! call rpcnotify({}, "Exit", v:exiting) | endif'.format(chanid))
 
-		// TODO? self.server.command('autocmd VimLeave * if exists("v:exiting") && v:exiting > 0 | silent! call rpcnotify({}, "Exit", v:exiting) | endif'.format(chanid))
-
-		if err := r.nvim.Command("augroup END"); err != nil {
+		if err := nv.Command("augroup END"); err != nil {
 			return err
 		}
 	}
-
-	// wait logic...
 
 loop:
 	for {
@@ -142,10 +148,6 @@ loop:
 	return nil
 }
 
-func (r *Runner) Close() error {
-	return r.nvim.Close()
-}
-
 func (r *Runner) wait() bool {
 	return r.remoteWait != ""
 }
@@ -154,4 +156,29 @@ func (r *Runner) addWait(n int) {
 	r.m.Lock()
 	r.waitCount += n
 	r.m.Unlock()
+}
+
+func (r *Runner) startNewNvim() error {
+	fmt.Fprintln(os.Stderr, "Starting new nvim process...")
+
+	env := os.Environ()
+	if r.address != "" {
+		env = append(env, fmt.Sprintf("NVIM_LISTEN_ADDRESS=%s", r.address))
+	}
+
+	binary := os.Getenv("NVIM_CMD")
+	if binary == "" {
+		path, err := exec.LookPath("nvim")
+		if err != nil {
+			return errors.New("Could not find executable path")
+		}
+		binary = path
+	}
+
+	args := flag.Args()
+	newArgs := make([]string, len(args)+1)
+	copy(newArgs[1:], args)
+	newArgs[0] = binary
+	fmt.Println("newArgs", newArgs)
+	return syscall.Exec(binary, newArgs, env)
 }
