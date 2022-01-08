3e571e782a26a0b520ab2548f29f1a6190514712
ykpythemind <yukibukiyou@gmail.com>
2022-01-08 17:01:00 +0900

    nnn

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ykpythemind/neovim-remote-go
 
 go 1.17
 
-require github.com/neovim/go-client v1.2.0 // indirect
+require (
+	github.com/creack/pty v1.1.17 // indirect
+	github.com/neovim/go-client v1.2.0 // indirect
+)
--- main_test.go
+++ main_test.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 	"testing"
 	"time"
+
+	"github.com/creack/pty"
 )
 
 var seededRand *rand.Rand = rand.New(
@@ -22,26 +24,50 @@ func newCommand(cmd ...string) *exec.Cmd {
 }
 
 func newNvim() *exec.Cmd {
+	// return exec.Command("nvim", "-nu", "NORC", "--headless")
+	// return exec.Command("nvim", "-nu", "NORC")
 	return exec.Command("nvim", "-nu", "NORC", "--headless")
 }
 
 func TestOpenFile(t *testing.T) {
+	f, err := os.Create("/tmp/test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
 	r := strconv.Itoa(seededRand.Intn(100000))
 	listenAddress := fmt.Sprintf("/tmp/neovim-remote-test_%s", r)
 	os.Setenv("NVIM_LISTEN_ADDRESS", listenAddress)
 
 	cmd := newNvim()
-	cmd.Env = []string{fmt.Sprintf("NVIM_LISTEN_ADDRESS=%s", listenAddress)}
+	cmd.Env = []string{
+		fmt.Sprintf("NVIM_LISTEN_ADDRESS=%s", listenAddress),
+		"LANG=en_US.UTF-8",
+	}
 
 	var outb, errb bytes.Buffer
 	cmd.Stdout = &outb
 	cmd.Stderr = &errb
 
-	cherr := make(chan error, 1)
+	// cherr := make(chan error, 1)
+	// chFile := make(chan *os.File, 1)
 
-	go func() {
-		cherr <- cmd.Run()
-	}()
+	// go func() {
+	// 	cherr <- cmd.Run()
+	// }()
+
+	ptmx, err := pty.Start(cmd)
+	// pty, tty, err := pty.Open(cmd)
+	if err != nil {
+		t.Error(err)
+	}
+	// Make sure to close the pty at the end.
+	defer func() { _ = ptmx.Close() }() // Best effort.
+
+	fmt.Println("wait start cmd...")
+	// ptmx := <-chFile
+	fmt.Println("ptmx started")
 
 	time.Sleep(3 * time.Second)
 
@@ -49,19 +75,107 @@ func TestOpenFile(t *testing.T) {
 	// nv := newCommand("")
 	Run("neovim-remote", "README.md")
 
-	time.Sleep(3 * time.Second)
+	// buf := make([]byte, 1024)
+	ptmx.Write([]byte("ifugafuga"))
+	ptmx.Write([]byte{27}) // ESC
 
-	if err := cmd.Process.Signal(os.Interrupt); err != nil {
-		t.Error(err)
+	// // 読み込んで表示
+	// n, err := ptmx.Read(buf)
+	// fmt.Println(string(buf[:n]))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// ptmx.Write([]byte(":q!"))
+	//
+	time.Sleep(1 * time.Second)
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	ptmx.Write([]byte("ggifugafuga"))
+
+	endgame := time.After(5 * time.Second)
+
+	// time.Sleep(1 * time.Second)
+
+	// 読み込んで表示
+	buf := make([]byte, 1024)
+	_, err = ptmx.Read(buf)
+	// fmt.Println(string(buf[:n]))
+	if err != nil {
+		panic(err)
 	}
+
+	// n := 0
 
 	select {
-	case err := <-cherr:
-		if err != nil {
-			fmt.Printf("errch: %s", err)
-			// t.Fatal(err)
-		}
+	case <-endgame:
+		fmt.Println("time to stop!")
+
+		// fmt.Println("read")
+		// nn, err := ptmx.Read(buf)
+		// n = nn
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		// ptmx.Write([]byte{27}) // ESC
+		// ptmx.Write([]byte{58}) // :
+		// ptmx.Write([]byte{'q'})
+		// ptmx.Write([]byte{33}) // !
+		// ptmx.Write([]byte{13}) // ENTER
+
+		ptmx.Write([]byte("%print"))
+		ptmx.Write([]byte{13}) // ENTER
+		ptmx.Write([]byte("q"))
+		ptmx.Write([]byte{33}) // !
+		ptmx.Write([]byte{13}) // ENTER
+
+		// buf := make([]byte, 1024)
+		// n, err := ptmx.Read(buf)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Println(string(buf[:n]))
+
+		time.Sleep(1 * time.Second)
+
+		// if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+		// 	t.Error(err)
+		// }
+
+		// if err := ptmx.Close(); err != nil {
+		// 	t.Fatal(err)
+		// }
+
+		// fmt.Println("out", outb.String())
+		// fmt.Println("err", errb.String())
+
+		// case <-ticker.C:
+		// 	fmt.Println("out", outb.String())
+		// 	fmt.Println("err", errb.String())
 	}
 
-	fmt.Println("out:", outb.String(), "err:", errb.String())
+	// err = cmd.Wait()
+	// if err != nil {
+	// 	t.Logf("err type %s", reflect.TypeOf(err))
+	// 	if e, ok := err.(*exec.ExitError); ok {
+	// 		fmt.Println("code", e.ExitCode())
+	// 		fmt.Println("stderr", e.Stderr)
+	// 	}
+	//
+	// 	str := outb.String()
+	// 	fmt.Fprint(f, str)
+	// 	fmt.Println("out", str)
+	// 	fmt.Println("err", errb.String())
+	// 	// t.Fatalf("cmd not finish %s", err)
+	// }
+	fmt.Println("finish...")
+
+	str := outb.String()
+	fmt.Fprint(f, str)
+	fmt.Println("out", str)
+	fmt.Println("err", errb.String())
+
 }
